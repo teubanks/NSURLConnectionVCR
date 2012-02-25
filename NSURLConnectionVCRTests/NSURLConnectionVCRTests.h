e1e0045ba693c02ea93887668b54a2da4ca6daa8
Martijn The <post@martijnthe.nl>
2012-02-25 13:07:35 +0100

    Initial Commit

---
+++ NSURLConnectionVCRTests/NSURLConnectionVCRTests.h
@@ -0,0 +1,13 @@
+//
+//  NSURLConnectionVCRTests.h
+//  NSURLConnectionVCRTests
+//
+//  Created by Martijn Thé on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSURLConnectionVCRTests : SenTestCase
+
+@end
---
+++ NSURLConnectionVCRTests/NSURLConnectionVCRTests.m
@@ -0,0 +1,32 @@
+//
+//  NSURLConnectionVCRTests.m
+//  NSURLConnectionVCRTests
+//
+//  Created by Martijn Thé on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSURLConnectionVCRTests.h"
+
+@implementation NSURLConnectionVCRTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSURLConnectionVCRTests");
+}
+
+@end
